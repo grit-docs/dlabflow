22b0e09e19635b695917e6718fc1a266eca6866f
silee <silee@gwnu.ac.kr>
2025-04-14 17:25:10 +0900

    no message

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,24 +13,15 @@ const config = {
   title: 'D-Lab Flow Docs',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
+  trailingSlash: false,
   url: 'https://grit-docs.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/dlabflow/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'grit', // Usually your GitHub org/user name.
-  projectName: 'dlabflow', // Usually your repo name.
-
+  organizationName: 'gritdevops',
+  projectName: 'dlabflow',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
